75bfdca0d5121fccb1a2c87902438f7770e72c97
Eetu Sjöblom <eetu.sjoblom@helsinki.fi>
2021-03-30 21:26:28 +0300

    doc batch prediction, sentence splitting, pretokenization and return formats

--- serve.py
+++ serve.py
@@ -3,16 +3,15 @@ import json
 import os
 import sys
 import unicodedata
-
-from flask import Flask, request
-
 import numpy as np
 import tensorflow as tf
 
+from flask import Flask, request
 from data import Word, Sentence, Document, Token, PREDICTION_SUMMARIZERS
 from example import EXAMPLE_GENERATORS, examples_to_inputs
 from label import LabelEncoder, IobesTokenLabeler, iobes_to_iob2, LABEL_ASSIGNERS
 from model import load_ner_model
+from mosestokenizer import MosesTokenizer, MosesSentenceSplitter
 
 
 app = Flask(__name__)
@@ -21,8 +20,10 @@ app = Flask(__name__)
 @app.route('/', methods=["POST"])
 def tag():
     data = request.get_json()
-    text = data['text']
-    return app.tagger.tag(text)
+    text = data["text"]
+    mode = data["mode"] if "mode" in data else "sentence"
+    output = data["output"] if "output" in data else "doc"
+    return app.tagger.tag(text, mode, output)
 
 
 class Tagger(object):
@@ -52,11 +53,25 @@ class Tagger(object):
         )
         self.summarize_predictions = PREDICTION_SUMMARIZERS[saved_args["summarize_preds"]]
 
-    def tag(self, text):
-        words = text.split()
-        docs = [Document([Sentence([Word(token, "O") for token in words])])]
+        self.splitter = MosesSentenceSplitter("fi")
+        self.pretokenizer = MosesTokenizer("fi")
 
-        docs[0].tokenize(self.tokenizer.tokenize, self.token_labeler.label_tokens)
+    def tag(self, text, mode, output):
+        if mode == "sentence":
+            words = self.pretokenizer(text)
+            docs = [Document([Sentence([Word(token, "O") for token in words])])]
+        elif mode == "doc":
+            sent_objs = []
+            sents = self.splitter([text])
+            for sent in sents:
+                sent_objs.append(Sentence([Word(token, "O") for token in self.pretokenizer(sent)]))
+            docs = [Document(sent_objs)]
+        else:
+            return "{}"
+
+        for d in docs:
+            d.tokenize(self.tokenizer.tokenize, self.token_labeler.label_tokens)
+
         examples = self.example_generator.examples(docs)
         examples = list(examples)
         x, y = examples_to_inputs(examples)
@@ -71,16 +86,28 @@ class Tagger(object):
             self.summarize_predictions(document)
             self.assign_labels(document, self.label_encoder)
 
-        return_words, return_labels = [], []
-        for sentence in docs[0].sentences:
-            for word in sentence.words:
-                return_labels.append(word.predicted_label)
-                return_words.append(word.text)
+        if output == "json":
+            return_words, return_labels = [], []
+            for sentence in docs[0].sentences:
+                return_words.append([]), return_labels.append([])
+                for word in sentence.words:
+                    return_labels[-1].append(word.predicted_label)
+                    return_words[-1].append(word.text)
 
-        return json.dumps(
-            {"tokens": return_words, "labels": return_labels},
-            ensure_ascii=False
-        )
+            return json.dumps(
+                {"tokens": return_words, "labels": return_labels},
+                ensure_ascii=False
+            ) + "\n"
+        elif output == "tsv":
+            outs = ""
+            for i, sentence in enumerate(docs[0].sentences):
+                for word in sentence.words:
+                    outs += f"{word.text}\t{word.predicted_label}\n"
+                outs += "\n"
+            return outs
+        else:
+            return "{}"
+
 
     @classmethod
     def load(cls, model_dir):
